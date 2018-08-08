c9b07de7440caf5add4d9861cf6ebdc3504712e6
tiffahny <tiff9490@gmail.com>
2018-08-08 14:26:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 
 currentUser = "tiff"
-welcomeMessage = "Welcome to Klossybook, ";
+welcomeMessage = "Welcome to Klossybook, " + currentUser;
 excitedWelcomeMessage = "WELCOME TO KLOSSYBOOK, "
 shortGreeting = "Welcome, t!"
