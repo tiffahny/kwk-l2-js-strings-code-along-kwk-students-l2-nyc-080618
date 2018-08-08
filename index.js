5ef274dcd084345c78d899649083755ec52e887b
tiffahny <tiff9490@gmail.com>
2018-08-08 14:26:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 // Write your code in this file!
 
+currentUser = "tiff"
+welcomeMessage = "Welcome to Klossybook, " + currentUser + "!" ;
+excitedWelcomeMessage = "WELCOME TO KLOSSYBOOK, " + "TIFF" +"!"
+shortGreeting = "Welcome, t!"
